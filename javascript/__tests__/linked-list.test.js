276fd8b77640a7761ec60789266d76188f6bbf81
Misti <mistidinzy@gmail.com>
2022-10-20 17:39:41 -0500

    Can instantiate an empty linked list test passing

--- javascript/__tests__/linked-list.test.js
+++ javascript/__tests__/linked-list.test.js
@@ -9,3 +9,16 @@ describe('Node Test', () => {
     expect(node.next).toBeNull();
   });
 });
+
+let LinkedList = require('../linked-list/index');
+
+describe('LinkedList Tests', () => {
+  //----- CREATING A LINKED LIST -----//
+
+  // CC05-01. Can successfully instantiate an EMPTY linked list.
+  test('CC05-01: Can instantiate a new, empty Linked List.', () => {
+    let newList = new LinkedList();
+
+    expect(newList.head).toBeNull();
+  });
+})
