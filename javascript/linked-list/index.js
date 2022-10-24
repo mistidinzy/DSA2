2781b66316b5050e7d7bfd07323af3c70e9ca483
Misti <mistidinzy@gmail.com>
2022-10-24 10:43:33 -0500

    append method

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -38,7 +38,20 @@ class LinkedList {
     return str;
   }
 
-  
+  //Inserts a new node to the END of the list.
+  append(value) {
+    if (!this.head) {
+      this.head = new Node(value);
+      return;
+    }
+
+    let current = this.head;
+
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = new Node(value);
+  }
 
 
 
