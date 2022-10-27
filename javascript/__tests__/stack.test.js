39a6a964055b9e1a8f87b37671fcf989897fb61b
Misti <mistidinzy@gmail.com>
2022-10-26 22:03:16 -0500

    separates test into two, to test opposite outcomes, tests passing

--- javascript/__tests__/stack.test.js
+++ javascript/__tests__/stack.test.js
@@ -78,18 +78,19 @@ describe('Stack Tests', () => {
 
   //-----ADDITIONAL TESTS-----//
 
-  test('CC10-S.08: isEmpty returns true if stack is empty.', () => {
-    let stack = new Stack().fromArray([3,4,5,6]);
-
+  test('CC10-S.08: isEmpty returns TRUE if stack IS empty.', () => {
+    let stack = new Stack();
     let result = stack.isEmpty();
-    expect(result).toBeFalsy();
-
-    stack.clear();
-    let result2 = stack.isEmpty();
-    expect(result2).toBeTruthy();
+    expect(result).toBeTruthy();
   });
 
-  test('CC10-S.09: Returns Stack as a string.', () => {
+  test('CC10-S.09: isEmpty returns FALSE if stack is NOT empty.', () => {
+    let stack = new Stack().fromArray([3,4,5,6]);
+    let result = stack.isEmpty();
+    expect(result).toBeFalsy();
+  });
+
+  test('CC10-S.10: Returns Stack as a string.', () => {
     let stack = new Stack();
     stack.push(3);
     stack.push(2);
@@ -97,17 +98,17 @@ describe('Stack Tests', () => {
     expect(stack.toString()).toEqual('T -> 1| 2| 3| -> N');
   });
 
-  test('CC10-S.10: Creates a stack from an array, in SAME index order as array (top = i @ 0).', () => {
+  test('CC10-S.11: Creates a stack from an array, in SAME index order as array (top = i @ 0).', () => {
     let stack = new Stack().fromArray([1,2,3,4,5]);
     expect(stack.toString()).toEqual('T -> 1| 2| 3| 4| 5| -> N');
   });
 
-  test('CC10-S.11: Creates a stack from an array, in REVERSE index order as array (top = i @ end).', () => {
+  test('CC10-S.12: Creates a stack from an array, in REVERSE index order as array (top = i @ end).', () => {
     let stack = new Stack().reverseFromArray([1,2,3,4,5]);
     expect(stack.toString()).toEqual('T -> 5| 4| 3| 2| 1| -> N');
   });
 
-  test('CC10-S.12: Correctly updates/returns size of stack.', () => {
+  test('CC10-S.13: Correctly updates/returns size of stack.', () => {
     let stack = new Stack().fromArray([1,2,3,4,5]);
     expect(stack.count).toEqual(5);
     stack.push(3);
@@ -116,7 +117,7 @@ describe('Stack Tests', () => {
     expect(stack.count).toEqual(5);
   });
 
-  test('CC10-S.13: Can successfully clear current stack.', () => {
+  test('CC10-S.14: Can successfully clear current stack.', () => {
     let stack = new Stack().fromArray([1,2,3,4,5]);
     expect(stack.count).toEqual(5);
     stack.pop();
