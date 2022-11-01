bfef86869198a6fd501697f9ecf5f93351c7a275
Misti <mistidinzy@gmail.com>
2022-11-01 16:54:20 -0500

    creates PseudoQueue

--- javascript/stack-and-queue/pseudo-queue.js
+++ javascript/stack-and-queue/pseudo-queue.js
@@ -1,2 +1,26 @@
 'use strict';
 
+const Stack = require("./stack");
+
+class PseudoQueue{
+  constructor(){
+    this.inStack = new Stack();
+    this.outStack = new Stack();
+  }
+
+  enqueue(value){
+    this.inStack.push(value);
+  }
+
+  dequeue(){
+    if(this.inStack.isEmpty){
+      throw new Error('The queue is already empty!');
+    }else{
+      let p = this.inStack.pop();
+      this.outStack.push(p);
+    }
+    return this.outStack.push(p);
+  }
+}
+
+module.exports = PseudoQueue;
