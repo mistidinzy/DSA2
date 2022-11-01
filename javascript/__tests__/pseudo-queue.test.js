b17b1b689404b346a3d9971974b84f7bf2012b17
Misti <mistidinzy@gmail.com>
2022-11-01 16:58:10 -0500

    Can create an empty PseudoQueue tests passing

--- javascript/__tests__/pseudo-queue.test.js
+++ javascript/__tests__/pseudo-queue.test.js
@@ -6,6 +6,9 @@ describe('Pseudo Queue Tests', () => {
   test('It can create an empty Pseudo Queue', () => {
     let pQ = new PseudoQueue();
 
-    expect(pQ.top).toBeNull();
+    expect(pQ.inStack.top).toBeNull();
+    expect(pQ.outStack.top).toBeNull();
+    expect(pQ.inStack.count).toEqual(0);
+    expect(pQ.outStack.count).toEqual(0);
   })
 });
