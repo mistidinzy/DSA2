c77cd253e7717a1f3af879a6e0e4bfd8636fa424
Misti <mistidinzy@gmail.com>
2022-11-14 13:00:44 -0600

    starts animal shelter test outline

--- javascript/__tests__/FIFO-shelter.test.js
+++ javascript/__tests__/FIFO-shelter.test.js
@@ -7,7 +7,9 @@ describe("FIFO - Animal Shelter Tests", () => {
   test('01. Can create a new, empty Animal Shelter object.', () => {
     let shelter = new AnimalShelter();
 
-    shelter.enqueue(new Dog());
+    let result = shelter.enqueue(new Dog());
+
+    console.log(result);
   })
 
 });
