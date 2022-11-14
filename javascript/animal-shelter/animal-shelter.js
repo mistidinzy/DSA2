b994708417ee60f5dfd971c2712af30bfc9e8f60
Misti <mistidinzy@gmail.com>
2022-11-14 12:58:36 -0600

    Adds AnimalShelter methods

--- javascript/animal-shelter/animal-shelter.js
+++ javascript/animal-shelter/animal-shelter.js
@@ -1,27 +1,67 @@
-'use strict';
+"use strict";
 
-let Queue = require('../stack-and-queue/queue');
+let Queue = require("../stack-and-queue/queue");
 
-class Dog{
-  constructor(){
-    this.nickname = '';
+class Dog {
+  constructor() {
+    this.nickname = "";
     this.intake = new Date().toDateString();
     this.status = false;
   }
 }
 
-class Cat{
-  constructor(){
-    this.nickname = '';
+class Cat {
+  constructor() {
+    this.nickname = "";
     this.intake = new Date().toDateString();
     this.status = false;
   }
 }
 
-class AnimalShelter{
-  constructor(){
+class AnimalShelter {
+  constructor() {
     this.dogs = new Queue();
     this.cats = new Queue();
+  }
+
+  enqueue(animal) {
+    if (animal == Dog) {
+      this.dogs.enqueue(animal);
+    } else if (animal == Cat) {
+      this.cats.enqueue(animal);
+    }
+  }
+
+  dequeue(pref) {
+    if (pref == Dog) {
+      this.dogs.dequeue();
+    } else if (pref == Cat) {
+      this.cats.dequeue();
+    } else {
+      this.getLongest();
+    }
+  }
+
+  getLongest() {
+    let firstCat = this.cats.peek();
+    let firstDog = this.dogs.peek();
+
+    let firstCatIntake = firstCat.intake;
+    let firstDogIntake = firstDog.intake;
+
+    if (firstCatIntake >= firstDogIntake) {
+      this.cats.dequeue();
+    } else {
+      this.dogs.dequeue();
+    }
+  }
+
+  newName(animal, newName) {
+    if (animal == Dog) {
+      this.nickname = newName;
+    } else if (animal == Cat) {
+      this.nickname = newName;
+    }
   }
 }
 
