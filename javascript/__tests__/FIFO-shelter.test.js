8fa72fdd7c4c80caa4abc3a0115dc59758c098ec
Misti <mistidinzy@gmail.com>
2022-11-17 22:26:54 -0600

    can change an intake date, test passing

--- javascript/__tests__/FIFO-shelter.test.js
+++ javascript/__tests__/FIFO-shelter.test.js
@@ -11,9 +11,9 @@ describe("FIFO - Animal Shelter Tests", () => {
 
   test("02. Can add a Dog to the Animal Shelter.", () => {
     let shelter = new AnimalShelter();
-    shelter.newDog("Fido");
-    shelter.newDog("Steve");
-    shelter.newDog("Cheeseball");
+    shelter.newPet("Fido", "dog");
+    shelter.newPet("Steve", "dog");
+    shelter.newPet("Cheeseball", "dog");
     expect(shelter.dogs.front.value.nickname).toEqual("Fido");
     expect(shelter.dogs.front.next.value.nickname).toEqual("Steve");
     expect(shelter.dogs.back.value.nickname).toEqual("Cheeseball");
@@ -21,79 +21,77 @@ describe("FIFO - Animal Shelter Tests", () => {
 
   test("03. Can add a Cat to the Animal Shelter.", () => {
     let shelter = new AnimalShelter();
-    shelter.newCat("Mr.Meowgi");
-    shelter.newCat("Noodle");
+    shelter.newPet("Mr.Meowgi", "cat");
+    shelter.newPet("Noodle", "cat");
     expect(shelter.cats.front.value.nickname).toEqual("Mr.Meowgi");
     expect(shelter.cats.back.value.nickname).toEqual("Noodle");
   });
 
   test("04. Can dequeue a pet based on preference (Cat).", () => {
     let shelter = new AnimalShelter();
-    shelter.newCat("Mr.Meowgi");
-    shelter.newDog("Steve");
-    shelter.newCat("Noodle");
-    shelter.newDog("Cheeseball");
-    let result = shelter.dequeue("cat");
+    shelter.newPet("Mr.Meowgi", "cat");
+    shelter.newPet("Steve", "dog");
+    shelter.newPet("Noodle", "cat");
+    shelter.newPet("Cheeseball", "dog");
+    shelter.newPet("Pancake", "dog");
+    let result = shelter.adopt("cat");
     expect(result.value.nickname).toEqual("Mr.Meowgi");
-    let result2 = shelter.dequeue("cat");
+    let result2 = shelter.adopt("cat");
     expect(result2.value.nickname).toEqual("Noodle");
   });
 
   test("05. Can dequeue a pet based on preference (Dog).", () => {
     let shelter = new AnimalShelter();
-    shelter.newCat("Mr.Meowgi");
-    shelter.newDog("Steve");
-    shelter.newCat("Noodle");
-    shelter.newDog("Cheeseball");
-    let result = shelter.dequeue("dog");
+    shelter.newPet("Mr.Meowgi", "cat");
+    shelter.newPet("Steve", "dog");
+    shelter.newPet("Noodle", "cat");
+    shelter.newPet("Cheeseball", "dog");
+    shelter.newPet("Pancake", "dog");
+
+    let result = shelter.adopt("dog");
     expect(result.value.nickname).toEqual("Steve");
-    let result2 = shelter.dequeue("dog");
+    let result2 = shelter.adopt("dog");
     expect(result2.value.nickname).toEqual("Cheeseball");
-    expect(() => {
-      shelter.dequeue();
-    }).toThrow(Error);
   });
 
-  test("06. Can update an intake date.", () => {
+  test("06. Can get simplified array of all pets in shelter with getAll.", () => {
     let shelter = new AnimalShelter();
-    shelter.newCat("Mr.Meowgi");
-    shelter.newDog("Steve");
-    shelter.newCat("Noodle");
-    shelter.newDog("Cheeseball");
-
-    // console.log(shelter);
-    // console.log(shelter.allPets);
+    shelter.newPet("Mr.Meowgi", "cat");
+    shelter.newPet("Steve", "dog");
+    shelter.newPet("Noodle", "cat");
+    shelter.newPet("Cheeseball", "dog");
+    shelter.newPet("Pancake", "dog");
+    let result = [
+      "CAT: Mr.Meowgi 11/17/2022 false",
+      "DOG: Steve 11/17/2022 false",
+      "CAT: Noodle 11/17/2022 false",
+      "DOG: Cheeseball 11/17/2022 false",
+      "DOG: Pancake 11/17/2022 false",
+    ];
+    expect(shelter.getAll()).toEqual(result);
   });
 
   test("07. Dequeue without a preference returns pet with longest tenure.", () => {
     let shelter = new AnimalShelter();
-    shelter.newCat("Mr.Meowgi");
-    shelter.newDog("Steve");
-    shelter.newCat("Noodle");
-    shelter.newDog("Cheeseball");
-
-    // console.log(shelter);
-    // console.log(shelter.allPets);
-  });
-
-  test("08. Can get simplified array of all pets in shelter with getAll.", () => {
-    let shelter = new AnimalShelter();
-    shelter.newCat("Mr.Meowgi");
-    shelter.newDog("Steve");
-    shelter.newCat("Noodle");
-    shelter.newDog("Cheeseball");
+    shelter.newPet("Mr.Meowgi", "cat");
+    shelter.newPet("Steve", "dog");
+    shelter.newPet("Noodle", "cat");
+    shelter.newPet("Cheeseball", "dog");
     shelter.newPet("Pancake", "dog");
 
-    console.log(shelter.getAll());
 
-    // let result = [
-    //   "CAT: Mr.Meowgi - Intake Date: Thu Nov 17 2022 - Spayed/Neutered: false",
-    //   "DOG: Steve - Intake Date: Thu Nov 17 2022 - Spayed/Neutered: false",
-    //   "CAT: Noodle - Intake Date: Thu Nov 17 2022 - Spayed/Neutered: false",
-    //   "DOG: Cheeseball - Intake Date: Thu Nov 17 2022 - Spayed/Neutered: false",
-    //   "DOG: Pancake - Intake Date: Thu Nov 17 2022 - Spayed/Neutered: false",
-    // ];
+  });
 
-    // expect(shelter.getAll()).toEqual(result);
+  test("08. Can update an intake date.", () => {
+    let shelter = new AnimalShelter();
+    shelter.newPet("Mr.Meowgi", "cat");
+    shelter.newPet("Steve", "dog");
+    shelter.newPet("Noodle", "cat");
+    shelter.newPet("Cheeseball", "dog");
+
+    let d3 = shelter.newPet("Pancake", "dog");
+    d3.changeIntake('04/04/2014');
+    expect(d3.intake).toEqual('4/4/2014');
+
   });
 });
--- javascript/animal-shelter/animal-shelter.js
+++ javascript/animal-shelter/animal-shelter.js
@@ -1,77 +1,42 @@
-'use strict';
+"use strict";
 
-let Queue = require('../stack-and-queue/queue');
+let Queue = require("../stack-and-queue/queue");
 
-class Pet{
-  constructor(){
-    this.nickname = '';
+class Pet {
+  constructor() {
+    this.nickname = "";
     this.intake = new Date().toLocaleDateString();
     this.status = false;
-    this.species = '';
+    this.species = "";
+  }
+
+  getName() {
+    return this.nickname;
   }
 
   newName(value) {
-    return this.nickname = value;
+    this.nickname = value;
+    return this.nickname;
   }
 
-  gotFixed() {
-    return this.status = true;
+  getIntake() {
+    return this.intake;
   }
 
-  changeIntake(value){
+  changeIntake(value) {
     let d = new Date(value);
     this.intake = d.toLocaleDateString();
     return this.intake;
   }
-}
 
-class Dog {
-  constructor() {
-    this.nickname = '';
-    this.intake = null;
-    this.status = false;
-    this.species = 'dog';
-  }
-
-  newName(value) {
-    this.nickname = value;
-    return this.nickname;
-  }
-
-  gotFixed() {
+  gotAltered() {
     this.status = true;
     return this.status;
   }
 
-  changeIntake(value){
-    let d = new Date(value);
-    this.intake = d.toDateString();
-    return this.intake;
-  }
-}
-
-class Cat {
-  constructor() {
-    this.nickname = '';
-    this.intake = null;
-    this.status = false;
-    this.species = 'cat';
-  }
-
-  newName(value) {
-    this.nickname = value;
-    return this.nickname;
-  }
-
-  gotFixed() {
-    this.status = true;
-    return this.status;
-  }
-
-  changeIntake(value){
-    let d = new Date(value);
-    this.intake = d.toDateString();
-    return this.intake;
+  editSpecies(s) {
+    this.species = s;
+    return this.species;
   }
 }
 
@@ -82,73 +47,163 @@ class AnimalShelter {
     this.allPets = [];
   }
 
-  newPet(name, species){
+  newPet(name, species) {
     let p = new Pet();
     p.nickname = name;
     p.intake = new Date().toLocaleDateString();
     p.species = species;
-    this.enqueue(p);
+    this.enQ(p);
     return p;
   }
 
-  enqueue(p){
-    if(p.species == 'cat'){
+  enQ(p) {
+    if (p.species == "cat") {
       this.cats.enqueue(p);
       this.allPets.push(p);
       return p;
-    }else if(p.species == 'dog'){
+    } else if (p.species == "dog") {
       this.dogs.enqueue(p);
       this.allPets.push(p);
-      return(p);
+      return p;
     }
   }
 
-  newDog(name) {
-    let d = new Dog();
-    d.nickname = name;
-    d.intake = new Date().toLocaleDateString();
-    this.allPets.push(d);
-    this.dogs.enqueue(d);
+  adopt(pref) {
+    if (pref == "dog") {
+      return this.dogs.dequeue();
+    } else if (pref == "cat") {
+      return this.cats.dequeue();
+    } else throw new Error("Please choose a pet.");
+  }
+
+  adoptDog() {
+    let d = this.dogs.dequeue();
     return d;
   }
 
-  newCat(name) {
-    let c = new Cat();
-    c.nickname = name;
-    c.intake = new Date().toLocaleDateString();
-    this.allPets.push(c);
-    this.cats.enqueue(c);
+  adoptCat() {
+    let c = this.cats.dequeue();
     return c;
   }
 
-  dequeue(pref){
-    if(pref == 'dog'){
-      let d = this.dogs.dequeue();
-
-      return d;
-    }
-    else if(pref == 'cat'){
-      let c = this.cats.dequeue();
-      return c;
-    }
-    else{
-      throw new Error('Please choose a dog or a cat.');
-    }
-  }
-
-  // getLongest() {
-
-
-  //   //!!if number is smaller, then tenure is longer!!//
-
-  // }
-
-  getAll(){
+  getAll() {
     let arr1 = this.allPets;
     let arr2 = [];
-    arr1.forEach(element => { arr2.push(`${element.species} ${element.nickname} ${element.intake} ${element.status}`);});
+    arr1.forEach((element) => {
+      arr2.push(
+        `${element.species.toUpperCase()}: ${element.nickname} ${
+          element.intake
+        } ${element.status}`
+      );
+    });
     return arr2;
+  }
+
+  findPet(p) {
+    let arr = this.allPets;
+    let arr2 = [];
+    let index;
+    let found;
+
+    for (let i = 0; i < arr.length; i++) {
+      arr2.push(arr[i].nickname);
+    }
+
+    let result = arr2.includes(p);
+
+    if (result == true) {
+      index = arr2.indexOf(p);
+      found = arr[index];
+      return found;
+    } else return "Not Found";
+  }
+
+  findPetIndex(p) {
+    let arr = this.allPets;
+    let arr2 = [];
+    let index;
+    let found;
+
+    for (let i = 0; i < arr.length; i++) {
+      arr2.push(arr[i].nickname);
+    }
+
+    let result = arr2.includes(p);
+
+    if (result == true) {
+      index = arr2.indexOf(p);
+      found = arr[index];
+      return arr.indexOf(found);
+    } else return "Not Found";
   }
 }
 
-module.exports = AnimalShelter, Dog, Cat;
+module.exports = AnimalShelter;
+
+// class Dog {
+//   constructor() {
+//     this.nickname = '';
+//     this.intake = null;
+//     this.status = false;
+//     this.species = 'dog';
+//   }
+
+//   newName(value) {
+//     this.nickname = value;
+//     return this.nickname;
+//   }
+
+//   gotFixed() {
+//     this.status = true;
+//     return this.status;
+//   }
+
+//   changeIntake(value){
+//     let d = new Date(value);
+//     this.intake = d.toDateString();
+//     return this.intake;
+//   }
+// }
+
+// class Cat {
+//   constructor() {
+//     this.nickname = '';
+//     this.intake = null;
+//     this.status = false;
+//     this.species = 'cat';
+//   }
+
+//   newName(value) {
+//     this.nickname = value;
+//     return this.nickname;
+//   }
+
+//   gotFixed() {
+//     this.status = true;
+//     return this.status;
+//   }
+
+//   changeIntake(value){
+//     let d = new Date(value);
+//     this.intake = d.toDateString();
+//     return this.intake;
+//   }
+// }
+
+// newDog(name) {
+//   let d = new Dog();
+//   d.nickname = name;
+//   d.intake = new Date().toLocaleDateString();
+//   this.allPets.push(d);
+//   this.dogs.enqueue(d);
+//   return d;
+// }
+
+// newCat(name) {
+//   let c = new Cat();
+//   c.nickname = name;
+//   c.intake = new Date().toLocaleDateString();
+//   this.allPets.push(c);
+//   this.cats.enqueue(c);
+//   return c;
+// }
